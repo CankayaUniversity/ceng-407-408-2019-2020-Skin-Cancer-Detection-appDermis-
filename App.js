4e08a38d00e157744a30975d463b415da1c04cf5
baasakc <baasakc@gmail.com>
2020-03-30 16:22:44 +0300

    fix

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import Login from './src/pages/Login'
 
+const Stack = createStackNavigator();
+
 export default class App extends Component {
-  render () {
-    return <Login />
-  }
+    render() {
+        return (
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen
+                        name="Login"
+                        component={Login}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+        );
+    }
 }
